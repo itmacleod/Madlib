a28b61c8a6e024db86a91365efa60ec243a5c5ae
Ian MacLeod <imac@Ians-MBP.fios-router.home>
2020-12-08 08:26:10 -0500

    Began working on GUI

--- src/madlibGUI/Madlib.java
+++ src/madlibGUI/Madlib.java
@@ -1,10 +1,26 @@
 package madlibGUI;
 
-public class Madlib {
+import javax.swing.*;
+import java.awt.Color;
+import java.awt.Font;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class Madlib extends JFrame{
+	public Madlib() {
+		getContentPane().setForeground(Color.WHITE);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("Ian MacLeod's GUI Madlibs");
+		setBackground(Color.WHITE);
+		getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("IAN MACLEOD'S WHACKY MADLIBS APP\n");
+		lblNewLabel.setFont(new Font("Times New Roman", Font.BOLD, 16));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(58, 32, 332, 20);
+		getContentPane().add(lblNewLabel);
 	}
 
+	public static void main(String[] args) {
+		
+
+	}
 }
